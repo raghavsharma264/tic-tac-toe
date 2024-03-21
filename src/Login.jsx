9cf417a87908891cbcce466d75be135ef361d7ad
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-03-21 23:53:40 +0530

    Login.jsx added
    User account functionality added.

---
+++ src/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const Login = () => {
+  return (
+    <>
+      <div>
+        <h1>Login</h1>
+      </div>
+    </>
+  );
+};
+
+export default Login;

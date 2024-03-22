e72d3b665d0e2033efbdb88e95bb935c565eabdb
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-03-22 23:46:05 +0530

    Signup.jsx updated
    Signup.jsx updated.

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,7 +3,25 @@ import React from "react";
 function Signup() {
   return (
     <>
-      <div>Signup</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: 100,
+          padding: 20,
+        }}
+      >
+        <Typography level="h4">
+          Welcome to Tic Yac Toe. Sign up below.
+        </Typography>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      ></div>
     </>
   );
 }

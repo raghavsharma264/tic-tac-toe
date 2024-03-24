d1ad9b20b7d5b37de3bc7444579c9594da50fc8e
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-03-24 23:33:01 +0530

    Home.jsx added
    Home.jsx added.

---
+++ src/Home.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+import "./App.css";
+
+function SquareButton({ value, onClick }) {
+  return (
+    <button onClick={onClick} className="squareButton">
+      {value}
+    </button>
+  );
+}
+
+function Home() {
+
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default Home;

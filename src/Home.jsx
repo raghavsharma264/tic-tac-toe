f3410c31e1b22e0ed2dd79256f9c8752263e8320
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-03-24 23:34:09 +0530

    Home.jsx updated
    Home.jsx updated.

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,6 +13,17 @@ function SquareButton({ value, onClick }) {
 }
 
 function Home() {
+  const [square, setSquare] = useState(Array(9).fill(""));
+  const [user1Turn, setUser1Turn] = useState(true);
+  const [status, setStatus] = useState("");
+  const [user1Score, setUser1Score] = useState(0);
+  const [user2Score, setUser2Score] = useState(0);
+  const [user1Name, setUser1Name] = useState("Player 1");
+  const [user2Name, setUser2Name] = useState("Player 2");
+  const [user1Input, setUser1Input] = useState("X");
+  const [user2Input, setUser2Input] = useState("O");
+
+
 
   return (
     <>

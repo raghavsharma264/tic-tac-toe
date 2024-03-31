adc4aa901e048d719b202bed71074d620a1a166e
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-03-31 22:24:35 +0530

    Home.jsx updated
    Home.jsx updated.

--- src/Home.jsx
+++ src/Home.jsx
@@ -43,6 +43,20 @@ function Home() {
     setUser2Input(event2.target.value);
   };
 
+  function checkWinner(square) {
+    const winnerValue = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+    ];
+
+  }
+
   return (
     <>
       <div className="header">

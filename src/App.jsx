c15f193f8993148fe951e55161d2b49702cf3bd4
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-04-22 21:49:04 +0530

    App.jsx updated
    App.jsx updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Login from "./Login";
-import Signup from "./Signup";
+import { useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
-const App = () => {
+import "./App.css";
+
+
+function App() {
+
+
   return (
-    <div>
-      <div>Login</div>
+    <>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </div>
+    </>
   );
-};
+}
 
 export default App;

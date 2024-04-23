d7db2614f299d8fb4df870711352601ec013bef6
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-04-23 23:16:08 +0530

    App.jsx updated
    App.jsx updated.

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,16 @@ function SquareButton({ value, onClick }) {
 }
 
 function App() {
+  const [square, setSquare] = useState(Array(9).fill(""));
+  const [user1Turn, setUser1Turn] = useState(true);
+  const [status, setStatus] = useState("");
+  const [user1Score, setUser1Score] = useState(0);
+
+
 
 
   return (
     <>
-
     </>
   );
 }

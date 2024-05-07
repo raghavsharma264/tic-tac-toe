2e7db777c58950529857923c1bec6337eff232eb
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-05-07 23:56:38 +0530

    App.jsx updated
    App.jsx updated.

--- src/App.jsx
+++ src/App.jsx
@@ -181,6 +181,14 @@ function App() {
                 padding: 30,
               }}
             >
+              <TextField
+                id="username-email"
+                sx={{ width: 300 }}
+                label="Username or Email"
+                variant="outlined"
+                value={usernameOrEmail}
+                onChange={handleUsernameOrEmailChange}
+              />
             </div>
           </form>
         </Card>

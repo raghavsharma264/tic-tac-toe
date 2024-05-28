db793b3b62e7b3044ba72a11d17d6d640a959bbd
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-05-28 23:48:15 +0530

    App.jsx updated
    App.jsx updated.

--- src/App.jsx
+++ src/App.jsx
@@ -213,6 +213,10 @@ function App() {
         </div>
         <div className="score">
           <p className="pstatus">
+            {user1Name} score: {user1Score}
+          </p>
+          <p className="pstatus">
+            {user2Name} score: {user2Score}
           </p>
         </div>
       </div>

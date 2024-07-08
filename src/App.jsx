8d7f514914a7571036a18f2ba7fe46d7f72c39e2
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-07-08 23:37:40 +0530

    App.jsx updated
    App.jsx updated.

--- src/App.jsx
+++ src/App.jsx
@@ -299,70 +299,16 @@ function App() {
               <TextField
                 type="password"
                 id="password"
-                sx={{ width: 300 }}
+                
+                
               />
+              <br />
+              <br />
             </div>
           </form>
         </Card>
       </div>
 
-      <div>
-        <Typography variant="h4">
-          Welcome to CodeCourse. Sign up below.
-        </Typography>
-      </div>
-      <Card variant="outlined" sx={{ maxWidth: 400 }}>
-        <div style={{ width: 300, padding: 30 }}>
-          <TextField
-            id="username"
-            sx={{ width: 300 }}
-            label="Username"
-            variant="outlined"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-          <br />
-          <br />
-          <TextField
-            type="email"
-            id="email"
-            sx={{ width: 300 }}
-            label="Email"
-            variant="outlined"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-          <br />
-          <br />
-          <TextField
-            type="password"
-            id="password"
-            sx={{ width: 300 }}
-            label="Password"
-            variant="outlined"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-          <br />
-          <br />
-          <TextField
-            type="password"
-            id="confirmPassword"
-            sx={{ width: 300 }}
-            label="Confirm Password"
-            variant="outlined"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-          />
-          <br />
-          <br />
-          <Button variant="contained">Sign up</Button>
-        </div>
-      </Card>
     </>
   );
 }
